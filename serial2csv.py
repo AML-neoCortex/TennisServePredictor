4606c27894c90d39e7212999cf1b84aebedeadfe
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2023-02-22 19:39:21 +0000

    printing serial output

--- serial2csv.py
+++ serial2csv.py
@@ -2,17 +2,36 @@ import serial
 import time
 import csv
 
-ser = serial.Serial("COM3", 115200)
+input = {b'24:4c:ab:82:f6:40\r\n'
+b'6,0,7,218,40,7,-21,-3,-35,0,0,0,0,0,0,0,0,0,6,0,7\r\n'
+b'24:4c:ab:82:f6:40\r\n'
+b'6,0,7,218,40,7,-21,-3,-35,0,0,0,0,0,0,0,0,0,6,0,7\r\n'
+b'24:4c:ab:82:f6:40\r\n'
+b'6,0,7,218,40,7,-21,-3,-35,0,0,0,0,0,0,0,0,0,6,0,7\r\n'
+b'24:4c:ab:82:fc:2c\r\n'
+b'0,-2,9,358,-2,13,-1,33,-42,0,0,0,0,0,0,0,0,0,0,-2,9\r\n'
+b'24:4c:ab:82:fc:2c\r\n'
+b'0,-2,9,358,-2,13,-1,33,-42,0,0,0,0,0,0,0,0,0,0,-2,9\r\n'
+b'24:4c:ab:82:fc:2c\r\n'
+b'0,-2,9,358,-2,13,-1,33,-42,0,0,0,0,0,0,0,0,0,0,-2,9\r\n'
+b'24:4c:ab:82:fc:2c\r\n'
+b'0,-2,9,358,-2,13,1,31,-43,0,0,0,0,0,0,0,0,0,0,-2,9\r\n'
+b'24:4c:ab:82:fc:2c\r\n'
+b'0,-2,9,358,-2,13,1,31,-43,0,0,0,0,0,0,0,0,0,0,-2,9\r\n'
+b'24:4c:ab:82:fc:2c\r\n'
+b'0,-2,9,358,-2,13,1,31,-43,0,0,0,0,0,0,0,0,0,0,-2,9\r\n'
+b'24:4c:ab:82:f6:40\r\n'
+b'in\r\n'
+b'24:4c:ab:82:f6:40\r\n'
+b'save\r\n'}
+
+ser = serial.Serial("/dev/cu.usbserial-1430", 115200)
 ser.flushInput()
 
 while True:
-    try:
-        ser_bytes = ser.readline()
-        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
-        # with open("test_data.csv","a") as f:
-        #     writer = csv.writer(f,delimiter=",")
-        #     writer.writerow([time.time(),decoded_bytes])
-    except:
-        print("Keyboard Interrupt")
-        break
+    ser_bytes = ser.readline()
+    # decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+    print(ser_bytes)
+    # with open("test_data.csv","a") as f:
+    #     writer = csv.writer(f,delimiter=",")
+    #     writer.writerow([time.time(),decoded_bytes])
