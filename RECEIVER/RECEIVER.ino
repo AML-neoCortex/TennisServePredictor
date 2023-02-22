f11892ad9134703b30dc4138e73c33f896a00e26
SachaHakim20 <98891615+SachaHakim20@users.noreply.github.com>
2023-02-22 19:15:31 +0000

    receiver, hopefully last version

--- RECEIVER/RECEIVER.ino
+++ RECEIVER/RECEIVER.ino
@@ -12,6 +12,12 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
+char in[] = "in";
+char out[] = "out";
+char save[] = "save";
+char nosave[] = "nosv";
+
+
 // IMU module features data structure
 typedef struct struct_message {
     int id;
@@ -51,76 +57,35 @@ struct_message boardsStruct[2] = {board1, board2};
 // Callback function that will be executed when data is received
 void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
   char macStr[18];
-  Serial.print("Packet received from: ");
+  //Serial.print("Packet received from: ");
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.println(macStr);
-  memcpy(&myData, incomingData, sizeof(myData));
-  //Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
-  //Update the structures with the new incoming data
-  boardsStruct[myData.id-1].Xacc = myData.Xacc;
-  Serial.printf("Xacc value: %d \n", boardsStruct[myData.id-1].Xacc);
-  Serial.println();
-    boardsStruct[myData.id-1].Yacc = myData.Yacc;
-  Serial.printf("Yacc value: %d \n", boardsStruct[myData.id-1].Yacc);
-  Serial.println();
-    boardsStruct[myData.id-1].Zacc = myData.Zacc;
-  Serial.printf("Zacc value: %d \n", boardsStruct[myData.id-1].Zacc);
-  Serial.println();
-  //   boardsStruct[myData.id-1].Xori = myData.Xori;
-  // Serial.printf("Xori value: %d \n", boardsStruct[myData.Xori-1].Xori);
-  // Serial.println();
-  //   boardsStruct[myData.id-1].Yori = myData.Yori;
-  // Serial.printf("Yori value: %d \n", boardsStruct[myData.id-1].Yori);
-  // Serial.println();
-  //   boardsStruct[myData.id-1].Zori = myData.Zori;
-  // Serial.printf("Zori value: %d \n", boardsStruct[myData.id-1].Zori);
-  // Serial.println();
-  //   boardsStruct[myData.id-1].Xmag = myData.Xmag;
-  // Serial.printf("Xmag value: %d \n", boardsStruct[myData.id-1].Xmag);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Ymag = myData.Ymag;
-  // Serial.printf("Ymag value: %d \n", boardsStruct[myData.id-1].Ymag);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Zmag = myData.Zmag;
-  // Serial.printf("Zmag value: %d \n", boardsStruct[myData.id-1].Zmag);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Xgyro = myData.Xgyro;
-  // Serial.printf("Xgyro value: %d \n", boardsStruct[myData.id-1].Xgyro);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Ygyro = myData.Ygyro;
-  // Serial.printf("Ygyro value: %d \n", boardsStruct[myData.id-1].Ygyro);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Zgyro = myData.Zgyro;
-  // Serial.printf("Zgyro value: %d \n", boardsStruct[myData.id-1].Zgyro);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Xrot = myData.Xrot;
-  // Serial.printf("Xrot value: %d \n", boardsStruct[myData.id-1].Xrot);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Yrot = myData.Yrot;
-  // Serial.printf("Yrot value: %d \n", boardsStruct[myData.id-1].Yrot);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Zrot = myData.Zrot;
-  // Serial.printf("Zrot value: %d \n", boardsStruct[myData.id-1].Zrot);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Xlin = myData.Xlin;
-  // Serial.printf("Xlin value: %d \n", boardsStruct[myData.id-1].Xlin);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Ylin = myData.Ylin;
-  // Serial.printf("Ylin value: %d \n", boardsStruct[myData.id-1].Ylin);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Zlin = myData.Zlin;
-  // Serial.printf("Zlin value: %d \n", boardsStruct[myData.id-1].Zlin);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Xgrav = myData.Xgrav;
-  // Serial.printf("Xgrav value: %d \n", boardsStruct[myData.id-1].Xgrav);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Ygrav = myData.Ygrav;
-  // Serial.printf("Ygrav value: %d \n", boardsStruct[myData.id-1].Ygrav);
-  // Serial.println();
-  //     boardsStruct[myData.id-1].Zgrav = myData.Zgrav;
-  // Serial.printf("Zgrav value: %d \n", boardsStruct[myData.id-1].Zgrav);
-  // Serial.println();
+
+  if (!strcmp((char*)incomingData, in)){
+    Serial.printf("in"); 
+    Serial.println();
+  }
+  else if (!strcmp((char*)incomingData, out)){
+    Serial.printf("out");
+    Serial.println();
+  }
+  else if (!strcmp((char*)incomingData, nosave)){
+    Serial.printf("nosave");
+    Serial.println();
+  }
+  else if (!strcmp((char*)incomingData, save)){
+    Serial.printf("save");
+    Serial.println();
+  }
+  else{
+    memcpy(&myData, incomingData, sizeof(myData));
+    
+    
+    // Xacc, Yacc, Zacc, Xori, Yori, Zori, Xmag, Ymag ,Zmag, Xgyro, Ygyro, Zgyro, Xrot, Yrot, Zrot, Xlin,Ylin, Zlin, Xgrav, Ygrav, Zgrav); //21
+    Serial.printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",myData.Xacc, myData.Yacc, myData.Zacc, myData.Xori, myData.Yori, myData.Zori, myData.Xmag, myData.Ymag , myData.Zmag, myData.Xgyro, myData.Ygyro, myData.Zgyro, myData.Xrot, myData.Yrot, myData.Zrot, myData.Xlin,myData.Ylin, myData.Zlin, myData.Xgrav, myData.Ygrav, myData.Zgrav);
+    Serial.println();
+  }
 }
  
 
